b8ac610b5c8261fa8927222abcc7321456ae6cb1
yuto ichinose <yuto.0526.sano@gmail.com>
2022-05-19 23:55:08 +0900

    update:新コンポーネント用に書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MapItem from "./components/MapItem";
 
+const items = [
+  {top: '10%',left: '20%',width: '50px',height: '90px',text:'A'},
+  {top: '30%',left: '10%',width: '50px',height: '90px',text:'B'},
+  {top: '70%',left: '50%',width: '50px',height: '90px',text:'C'},
+  {top: '50%',left: '20%',width: '50px',height: '90px',text:'D'},
+]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {items.map((i,n) => <MapItem dataObj={i} key={n}/>)}
+    </>
   );
 }
 
