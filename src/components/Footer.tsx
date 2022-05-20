c1ed71d1899d008915acc07588d900707f2731a0
yudai <yudai201729@icloud.com>
2022-05-21 08:38:15 +0900

    feat:Footererの作成(#3)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-main-color text-gray-100  px-24 pt-16 pb-8">
+      <div className=" justify-between mb-24 flex flex-col md:flex-row">
+        <div className="inline-block pb-24 md-pb-0 md:mx-0 mx-auto">
+          <span className="text-3xl">WhereMap</span>
+        </div>
+        <div className="inline-block md:mx-0 mx-auto">
+          <nav className=" inline-block ">
+            <ul className="flex flex-col md:flex-row gap-6">
+              <li className="hover:text-gray-300">
+                <a href="/">利用規約</a>
+              </li>
+              <li className="hover:text-gray-300">
+                <a href="/">プライバシーポリシー</a>
+              </li>
+              <li className="hover:text-gray-300">
+                <a href="/">お問い合わせ</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div className="text-center md:text-right">
+        <small>copyright all received</small>
+      </div>
+    </footer>
+  );
+};
