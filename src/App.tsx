eb37f44ba7db9acdd10202e346489e450a42a01f
yudai <yudai201729@icloud.com>
2022-05-21 13:06:03 +0900

    fix:react-router追加のため編集

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Layout } from "./components/Layout";
+import { NotFound } from "./components/NotFound";
+import { Top } from "./components/Top";
+import "./index.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Layout>
+          <Routes>
+            <Route index element={<Top />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
