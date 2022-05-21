28a9ddfb314b1fb0573e7c0f13f3302e26cf181c
yudai <yudai201729@icloud.com>
2022-05-21 12:43:39 +0900

    fix:モバイル版のときの中央寄せ(#3)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export const Footer = () => {
+export const Footer = (): JSX.Element=> {
   return (
     <footer className="bg-main-color text-gray-100  px-24 pt-16 pb-8">
       <div className=" justify-between mb-24 flex flex-col md:flex-row">
-        <div className="inline-block pb-24 md-pb-0 md:mx-0 mx-auto">
+        <div className="inline-block pb-24 md-pb-0 text-center md:text-left">
           <span className="text-3xl">WhereMap</span>
         </div>
-        <div className="inline-block md:mx-0 mx-auto">
+        <div className="inline-block text-center md:text-left">
           <nav className=" inline-block ">
             <ul className="flex flex-col md:flex-row gap-6">
               <li className="hover:text-gray-300">
