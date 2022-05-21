5d2eb7e9de7b7aee5c368df86824e0ae96344d6c
yuto ichinose <yuto.0526.sano@gmail.com>
2022-05-21 15:55:21 +0900

    update: マップコンポーネントを自動スクロールするようにした

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import MapItem from "./MapItem";
 
 const Map = ({imageURL, items}) => {
     console.log(items);
     console.log(imageURL);
-    const style = {
+    // const [image, setImage] = useState(null)
+    // console.log(image);
+    const wrapstyle = {
         position:'relative',
+        left: '2%',
         backgroundColor:'#ffffff',
-        textAlign: 'center'
-
+        textAlign: 'center',
+        width: '50%',
+        height: '',
+        overflow:'auto'
     }
-    console.log(style);
+    const imgstyle = {
+        maxWidth: 'initial',
+        margin: 'auto',
+    }
+    // console.log(wrapstyle);
     return (
-        <div style={style}>
-            <img src={imageURL} useMap="#map"/>
+        <div style={wrapstyle}>
+            <img style={imgstyle} src={imageURL} useMap="#map"/>
             <map name="map">
                 {items.map((i,n) => <MapItem dataObj={i} key={n}/>)}
             </map>
