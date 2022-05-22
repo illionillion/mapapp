bb2f0f216356b337ab741cc019b2ffb008e02f39
kawa-work <k.kawakami.work@gmail.com>
2022-05-22 12:41:15 +0900

    style: 一行が長かったのでフォーマット

--- src/components/MapItem.tsx
+++ src/components/MapItem.tsx
@@ -1,18 +1,23 @@
-const MapItem = ({dataObj}:any):JSX.Element => {
-    const style = {
+const MapItem = ({ dataObj }: any): JSX.Element => {
+  const style = {};
+  let coordsList = "";
+  for (const i of dataObj.coordinates) {
+    for (const j of i) {
+      coordsList = `${coordsList},${j}`;
     }
-    let coordsList=''
-    for (const i of dataObj.coordinates) {
-        for (const j of i) {
-            coordsList=`${coordsList},${j}`
-        }
-    }
-    coordsList = coordsList.replace(',','')
-    console.log(coordsList);
-    return(
-        <area shape={dataObj.areaType} style={style} coords={coordsList} onClick={()=>{alert(dataObj.name+'\n'+dataObj.photoPath)}}></area>
-    )
+  }
+  coordsList = coordsList.replace(",", "");
+  console.log(coordsList);
+  return (
+    <area
+      shape={dataObj.areaType}
+      style={style}
+      coords={coordsList}
+      onClick={() => {
+        alert(dataObj.name + "\n" + dataObj.photoPath);
+      }}
+    ></area>
+  );
+};
 
-}
-
-export default MapItem
+export default MapItem;
