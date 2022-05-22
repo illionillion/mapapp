4d1c166a789424a4de8e1e5668f27c80ad94f363
kawa-work <k.kawakami.work@gmail.com>
2022-05-22 13:29:10 +0900

    feat: clickAreaをReduxで管理

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -26,7 +26,7 @@ const Map = ():JSX.Element => {
             {/* <p>{mapname}</p> */}
             <img style={imgstyle} onLoad = {(e) => ImageMap('img[usemap]')} src = {require(`../data/${imageURL}`)} useMap = "#map"/>
             <map name="map">
-                {items.map((i,n) => <MapItem dataObj={i} key={n}/>)}
+                {items.map((i,n) => <MapItem dataObj={{...i, id: n}} key={n}/>)}
             </map>
             {/* <div>{maptext}</div> */}
 
--- src/components/MapItem.tsx
+++ src/components/MapItem.tsx
@@ -1,3 +1,8 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { clickAreaSlice } from '../features/clickArea';
+import { AppState } from '../store';
+
 const MapItem = ({ dataObj }: any): JSX.Element => {
   const style = {};
   let coordsList = "";
@@ -8,14 +13,24 @@ const MapItem = ({ dataObj }: any): JSX.Element => {
   }
   coordsList = coordsList.replace(",", "");
   console.log(coordsList);
+	const { clickArea } = useSelector<
+		AppState,
+		{ clickArea: number; }
+	>((state) => ({
+		clickArea: state.clickArea.id,
+	}));
+	const dispatch = useDispatch();
+	const { setClickArea } = clickAreaSlice.actions;
   return (
     <area
       shape={dataObj.areaType}
       style={style}
       coords={coordsList}
       onClick={() => {
+				dispatch(setClickArea({id: dataObj.id}))
         alert(dataObj.name + "\n" + dataObj.photoPath);
       }}
+			alt={dataObj.name}
     ></area>
   );
 };
