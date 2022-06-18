5d4a4cf2a2ec409d977b3899da34cbec0a0e688a
yudai <yudai201729@icloud.com>
2022-06-18 19:44:19 +0900

    feat:facilityアイテムとreduxの連携

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,23 @@ import { NotFound } from "./components/NotFound";
 import { Top } from "./components/Top";
 import { FacilityDetail } from "./components/FacilityDetail";
 import "./index.css";
+import { useEffect } from "react";
+import { facilityMapper } from "./repository/facilityMapper";
+import { useDispatch } from "react-redux";
+import { Facility } from "./types/facilityItem.d";
+import { setFacility } from "./features/facilitySlice";
 
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(()=>{
+   facilityMapper().then((datas:Facility[]) => {
+    dispatch(setFacility(datas));
+   }).catch(err => {
+    throw err;
+   })
+  },[])
+
   return (
     <div>
       <BrowserRouter>
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,21 @@
 import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-
 import { clickAreaSlice, ClickAreaState } from './features/clickArea';
+import { facilitySlice } from './features/facilitySlice';
+import { Facility } from './types/facilityItem.d';
 
 export type AppState = {
   clickArea: ClickAreaState;
+  FacilityList: Facility[]
 };
 
 const rootReducer = combineReducers<AppState>({
   clickArea: clickAreaSlice.reducer,
+  FacilityList: facilitySlice.reducer,
 });
 
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({ 
+  reducer: rootReducer 
+});
 
 export default store;
